5a5968b0aaec20362a0eae240c67cbe825d10c98
Jumner <justin462@protonmail.com>
2025-08-10 12:10:40 -0400

    helix script sets git editor

--- car/contrast/helix-editor.sh
+++ car/contrast/helix-editor.sh
@@ -3,4 +3,5 @@ echo "RUNNING car/contrast/helix-editor.sh"
 sudo add-apt-repository ppa:maveonair/helix-editor -y
 sudo apt update -y
 sudo apt install helix -y
+git config --global core.editor hx
 
