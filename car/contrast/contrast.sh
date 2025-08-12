4600fdf4a23ed894fa13d30f0671dd91b6606688
Jumner <justin462@protonmail.com>
2025-08-11 21:14:12 -0400

    Updated AA module

--- car/contrast/contrast.sh
+++ car/contrast/contrast.sh
@@ -5,3 +5,4 @@ export GIT_USERNAME="Jumner"
 export GIT_EMAIL="justin462@protonmail.com"
 $ROOT/common/common.sh
 $ROOT/car/contrast/helix-editor.sh
+$ROOT/modules/android-auto-module/install/common/common.sh
